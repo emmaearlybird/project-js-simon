3a1b09abfee162c64b2da788f1ae957dda600a17
Emma Ruth Burns <eburns@brynmawr.edu>
2020-07-17 02:13:17 -0600

    Final push
    Final push!

--- site/buttons.js
+++ site/buttons.js
@@ -1,45 +1,109 @@
-//I've commented out some stuff in the loop at the bottom,
-//this was just me working on trying to get the counter to only
-//go on red
-
-let colorWeShouldPick = "red";
-
-let audiogreen = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener("click", function() {
-  audiogreen.play();
-  document.getElementById("buttonClicks").innerHTML = "green";
+let startButton = document.querySelector('.start');
+startButton.addEventListener("click", function(){
+  start();
 });
 
-let audioyellow = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-let yellowButton = document.querySelector('.simon-button.yellow');
-yellowButton.addEventListener("click", function() {
-  audioyellow.play();
-  document.getElementById("buttonClicks").innerHTML = "yellow";
-});
-
-let audioblue = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-let blueButton = document.querySelector('.simon-button.blue');
-blueButton.addEventListener("click", function() {
-  audioblue.play();
-  document.getElementById("buttonClicks").innerHTML = "blue";
-});
-
-let audiored = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-let redButton = document.querySelector('.simon-button.red');
-redButton.addEventListener("click", function() {
-  audiored.play();
-  redButtonClick = "red";
-  document.getElementById("buttonClicks").innerHTML = "red";
-    if (colorWeShouldPick == "red"){
-      alert("yay");
-    }
-});
-
-//button lights and click counter:
 let header = document.getElementById("buttons");
 let btns = header.getElementsByClassName("simon-button");
 let countClicks = 0;
+let buttonWeDidClick = "";
+let colorChoice = ["red","green","blue","yellow"];
+let randomColor = colorChoice[Math.floor(Math.random()*colorChoice.length)];
+let colorWeShouldPick = [];
+let failSound = new Audio("http://soundbible.com/grab.php?id=1830&type=mp3");
+
+let ii = 0;
+console.log(randomColor);
+colorWeShouldPick.push(randomColor);
+
+function start(){
+if (ii < colorWeShouldPick.length) {
+  document.getElementById("prompt").innerHTML = colorWeShouldPick[ii];
+
+//GREEN BUTTON
+let audiogreen = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+let greenButton = document.querySelector('.simon-button.green');
+greenButton.addEventListener("mousedown", function() {
+  audiogreen.play();
+  document.getElementById("buttonClicks").innerHTML = "green";
+    buttonWeDidClick = document.getElementById("buttonClicks").innerHTML;
+  if (colorWeShouldPick[ii] == buttonWeDidClick) {
+    countClicks++;
+    randomColor = colorChoice[Math.floor(Math.random()*colorChoice.length)];
+    colorWeShouldPick.push(randomColor);
+    ii++;
+    document.getElementById("prompt").innerHTML = colorWeShouldPick[ii];
+  } else {
+    document.getElementById("buttonOne").innerHTML = "Try Again";
+    document.getElementById("prompt").innerHTML = "nope";
+    failSound.play();
+  }
+});
+
+//RED BUTTON
+let audiored = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+let redButton = document.querySelector('.simon-button.red');
+redButton.addEventListener("mousedown", function() {
+  audiored.play();
+  buttonWeDidClick == colorWeShouldPick;
+  document.getElementById("buttonClicks").innerHTML = "red";
+    buttonWeDidClick = document.getElementById("buttonClicks").innerHTML;
+  if (colorWeShouldPick[ii] == buttonWeDidClick) {
+      countClicks++;
+      randomColor = colorChoice[Math.floor(Math.random()*colorChoice.length)];
+      colorWeShouldPick.push(randomColor);
+      ii++;
+      document.getElementById("prompt").innerHTML = colorWeShouldPick[ii];
+  } else {
+      document.getElementById("buttonOne").innerHTML = "Try Again";
+      document.getElementById("prompt").innerHTML = "nope";
+      failSound.play();
+    }
+});
+
+//YELLOW BUTTON
+let audioyellow = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+let yellowButton = document.querySelector('.simon-button.yellow');
+yellowButton.addEventListener("mousedown", function() {
+  audioyellow.play();
+  buttonWeDidClick = "yellow";
+  document.getElementById("buttonClicks").innerHTML = "yellow";
+    buttonWeDidClick = document.getElementById("buttonClicks").innerHTML;
+  if (colorWeShouldPick[ii] == buttonWeDidClick) {
+      countClicks++;
+      randomColor = colorChoice[Math.floor(Math.random()*colorChoice.length)];
+      colorWeShouldPick.push(randomColor);
+      ii++;
+      document.getElementById("prompt").innerHTML = colorWeShouldPick[ii];
+  } else {
+      document.getElementById("buttonOne").innerHTML = "Try Again";
+      document.getElementById("prompt").innerHTML = "nope";
+      failSound.play();
+  }
+});
+
+//BLUE BUTTON
+let audioblue = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+let blueButton = document.querySelector('.simon-button.blue');
+blueButton.addEventListener("mousedown", function() {
+  audioblue.play();
+  buttonWeDidClick = "blue";
+  document.getElementById("buttonClicks").innerHTML = "blue";
+    buttonWeDidClick = document.getElementById("buttonClicks").innerHTML;
+  if (colorWeShouldPick[ii] == buttonWeDidClick) {
+      countClicks++;
+      randomColor = colorChoice[Math.floor(Math.random()*colorChoice.length)];
+      colorWeShouldPick.push(randomColor);
+      ii++;
+      document.getElementById("prompt").innerHTML = colorWeShouldPick[ii];
+  } else {
+      document.getElementById("buttonOne").innerHTML = "Try Again";
+      document.getElementById("prompt").innerHTML = "nope";
+      failSound.play();
+  }
+});
+
+//button lights and click counter:
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("mousedown", function() {
     var current = document.getElementsByClassName("active");
@@ -47,12 +111,13 @@ for (var i = 0; i < btns.length; i++) {
       current[0].className = current[0].className.replace(" active", "");
     }
     this.className += " active";
-    //if (colorWeShouldPick == redButtonClick){
-      countClicks++;}
-    //console.log(countClicks +" clicks");
+    //countClicks++;
     document.getElementById("clicks").innerHTML = countClicks;
+    console.log(countClicks +" clicks");
   });
   btns[i].addEventListener("mouseup", function() {
     this.className = this.className.replace(" active", "");
   });
+}
+}
 }
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,58 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+
+
+
+
+
+
+/*window.onload = function() {
+let colorArray = ["Blue ", "Green", "Red", "Yellow"];
+let currentPrompt = colorArray[0];
+console.log(currentPrompt);
+document.getElementById("buttonClicks").innerHTML = currentPrompt;
+
+
+
+};*/
+
+/*{const colorArray = ["Blue", "Green", "Red", "Yellow"];
+const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+console.log("random color =>", randomColor);
+
+}*/
+
+/*if (currentPromt = colorArray[0]) {
+
+}
+  ....
+} else {
+
+}
+if else alert*/
+
+
+
+
+
+/*if currentPrompt == (color that was clicked on){
+  move on to colorArray++
+}*/
+//print out the first color on the screen
+
+//wait for a mouse click
+//if the person clicks on the blue, do the next one
+//if the person clicks a different one, game over
+
+
+/*let countClicks = 0;
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("mousedown", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    countClicks++;
+    console.log(countClicks +" clicks");*/
